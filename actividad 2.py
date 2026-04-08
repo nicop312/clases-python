62bdf3a5b4363371572f7def3264c55bcb549ee2
Nicolas_Avalos <e11886@ism.edu.ec>
2026-04-08 13:23:00 -0500

    actividad 2

---
+++ actividad 2.py
@@ -0,0 +1,6 @@
+first_name, last_name, country, age, is_married= "Nicolas", "Avalos" , "Ecuador", "16", "false"
+print( "first_name:", first_name)
+print( "last_name:", last_name)
+print( "country:", country)
+print("age:", age)
+print( "casado:", is_married)
