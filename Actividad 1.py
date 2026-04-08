3124f797029ac456ec884e9a00a60d6b1dd7cf3f
Nicolas_Avalos <e11886@ism.edu.ec>
2026-04-08 13:15:42 -0500

    Actividad 1

---
+++ Actividad 1.py
@@ -0,0 +1,38 @@
+#Actividad 1
+
+
+print("la suma es:", 3+4)
+print("la resta es:", 3-4)
+print("la multiplicaion es:", 3*4)
+print("el modulo es:", 3%4)
+print("la divicion es:",3/4) 
+print("la evansevantaja es:", 3**4)
+print("la operacion de division de suelo es:", 3//4)
+
+
+
+#actividad 3
+
+
+
+
+print("Mi nombre es:Nicolas")
+print("Mi apellido es:Avalos")
+print("Mi pais es Ecuador")
+print("estoy disfruntado de 30 dias de python")
+
+
+
+#actividad 4
+
+
+
+
+print(type(10)) 
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(("Asabeneh","Python", "finland")))
+print(type("Nicolas"))
+print(type("Avalos"))
+print(type("Ecuador "))
