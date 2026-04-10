5812964e170e18582213a686f55c31628c6d0e8a
Nicolas_Avalos <e11886@ism.edu.ec>
2026-04-10 08:24:18 -0500

    Variables

---
+++ Variables.py
@@ -0,0 +1,4 @@
+#Nicolas_Avalos 10/04/2026, Variables.py
+
+nombre, apellido,NombreCompleto,pais,ciudad,edad,anos,Estacasado,esVerdadero,luzencendida = "Nicolas", "Avalos", "Nicolas Emanuel Avalos Ruales", "Ecuador","Quito",16,"diploma 1", False,True,True
+
