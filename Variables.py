eb19a8bd1fd6b6c0a5c86bdcf329ffa0cd615bcd
Nicolas_Avalos <137640148+Nicop312@users.noreply.github.com>
2026-04-13 09:32:14 -0500

    9:31

--- Variables.py
+++ Variables.py
@@ -1,5 +1,5 @@
 #Nicolas_Avalos 10/04/2026, Variables.py
-12312321
+djaiwd
 
 nombre, apellido,NombreCompleto,pais,ciudad,edad,anos,Estacasado,esVerdadero,luzencendida = "Nicolas", "Avalos", "Nicolas Emanuel Avalos Ruales", "Ecuador","Quito",16,"diploma 1", False,True,True
 
